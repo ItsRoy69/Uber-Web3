e02c02495660f4e656060690a17e8f3422533317
Jyotirmoy Roy <jyotirmoyroy649@gmail.com>
2023-04-21 09:36:21 +0530

    updated
    Signed-off-by: Jyotirmoy Roy <jyotirmoyroy649@gmail.com>

--- studio/schemas/index.js
+++
@@ -1,5 +0,0 @@
-import { userSchema } from './userSchema'
-import { ridesSchema } from './ridesSchema'
-import { tripSchema } from './tripSchema'
-
-export const schemaTypes = [ridesSchema, tripSchema, userSchema]
